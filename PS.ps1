08f028f622bf1a52fa45e0c662c81137c008a6a8
AmiyaKumarRout <amiya.rout@honeywell.com>
2017-06-28 15:35:25 +0530

    Add files via upload

---
+++ PS.ps1
@@ -0,0 +1,9 @@
+
+write-host "Hello Amiya" 
+$strEID = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name 
+write-host "Your EID = "  $strEID 
+ 
+$env:ComputerName 
+ 
+ps | sort -p ws |select -last 5 
+
